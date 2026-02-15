2471b7fac12faed052b3519b22806d838513e9cf
Maloschnikow <123018032+Maloschnikow@users.noreply.github.com>
2026-02-15 17:49:34 +0100

    add type checking

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   modules: ['@nuxt/eslint'],
   devtools: { enabled: true },
   compatibilityDate: '2025-07-15',
+  typescript: {
+    typeCheck: true,
+  },
   eslint: {
     config: {
       stylistic: {
